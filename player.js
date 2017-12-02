6cda3cd1a0cc0d0c28b20812d3e5212255da044c
JonesAndrew <andrew.joe.jones@gmail.com>
2017-12-02 11:26:43 -0500

    uppercut

--- player.js
+++ player.js
@@ -68,6 +68,20 @@ function Player() {
                 { duration: 6, frame: 24, after: 'idle' },
             ],
             moveable: false
+        },
+        upper_cut: {
+            frames: [
+                { duration: 6, frame: 28 },
+                { duration: 6, frame: 29 },
+                { duration: 6, frame: 30, action: function(self) {
+                    self.vel.y = -5;
+                } },
+                { duration: 6, frame: 31 },
+                { duration: 6, frame: 32 },
+                { duration: 6, frame: 33, after: 'air_fall' },
+            ],
+            isAirState: true,
+            moveable: false,
         }
     }
 
@@ -84,13 +98,17 @@ function Player() {
 Player.prototype.hitGround = function() {
     Entity.prototype.hitGround.call(this);
 
-    if (this.behavior.frame.isAirState) {
+    if (this.states[this.behavior.state].isAirState) {
+        if (this.behavior.state == 'upper_cut') {
+            return;
+        }
+
         this.behavior.changeState('idle');
     }
 }
 
 Player.prototype.standardInput = function() {
-    if (this.behavior.frame.moveable != false) {
+    if (this.states[this.behavior.state].moveable != false) {
         if (Key.isDown(Key.RIGHT)) {
             this.vel.x += this.speed;
 
@@ -109,19 +127,28 @@ Player.prototype.standardInput = function() {
             this.behavior.changeState('idle');
         }
 
-        if (Key.pressed(Key.UP) && this.landed) {
-            this.vel.y = -5;
+        if (Key.pressed(Key.JUMP) && this.landed) {
             this.behavior.changeState('air_rise');
         }
 
-        if (this.landed && Key.pressed(Key.P)) {
-            this.behavior.changeState('punch');
+        if (this.landed) {
+            if (Key.pressed(Key.P)) {
+                if (Key.isDown(Key.UP)) {
+                    this.behavior.changeState('upper_cut');
+                } else {
+                    this.behavior.changeState('punch');
+                }
+            }
         }
     }
 }
 
 Player.prototype.update = function() {
     Key.update();
+
+    if (!this.landed && !this.states[this.behavior.state].isAirState) {
+        this.behavior.changeState('air_fall');
+    }
 
     if (this.vel.x > this.friction) {
         this.vel.x = Math.min(this.max_speed, this.vel.x - this.friction);
